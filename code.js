f8105723e585097e7d5aa62663fa385a1625b8fd
UIT-20521206 <20521206@gm.uit.edu.vn>
2023-09-21 10:58:11 +0700

    1

---
+++ code.js
@@ -0,0 +1,20 @@
+
+const cong = (a,b) => {
+    console.log(a+b) ;
+}
+const tru = (a,b) => {
+    console.log(a-b);
+}
+const nhan = (a,b) => {
+    console.log(a*b);
+}
+const chia = (a,b) => {
+    console.log(a/b);
+}
+
+let x=3;
+let y=1;
+cong(x,y);
+tru(x,y);
+nhan(x,y);
+chia(x,y);
